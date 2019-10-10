7bce6e27319c2da9d723bdf0fff2b0a33298acfd
Twodawn <48268804+Twodawn@users.noreply.github.com>
2019-10-10 16:14:16 +0900

    Rename week6/src/week6/Ex6_1.java to week6/Ex6_1.java

--- week6/Ex6_1.java
+++ week6/Ex6_1.java
@@ -3,7 +3,7 @@ package week6;
 public class Ex6_1 {
 
 	public static void main(String[] args) {
-		System.out.println("20161450 ÀÌ»õº®");
+		System.out.println("20161450 Ã€ÃŒÂ»ÃµÂºÂ®");
 		
 		int d = sum(1,2,3);
 		System.out.println("d = "+d);
