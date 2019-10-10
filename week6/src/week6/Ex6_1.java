3c9984e4342c4e8f00663a24e554364c711667aa
404 <404@172.17.203.138>
2019-10-10 16:12:29 +0900

    week6

--- week6/src/week6/Ex6_1.java
+++ week6/src/week6/Ex6_1.java
@@ -3,6 +3,8 @@ package week6;
 public class Ex6_1 {
 
 	public static void main(String[] args) {
+		System.out.println("20161450 ¿Ãªı∫Æ");
+		
 		int d = sum(1,2,3);
 		System.out.println("d = "+d);
 	}
