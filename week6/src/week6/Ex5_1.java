de28c4e07a824915f9880b6da051283c8963941c
404 <404@172.17.203.138>
2019-10-10 15:12:41 +0900

    week6

---
+++ week6/src/week6/Ex5_1.java
@@ -0,0 +1,13 @@
+package week6;
+
+public class Ex5_1 {
+
+	public static void main(String[] args) {
+		System.out.println("20161450 이새벽");
+		int a0=8; int a1=5; int a2=2; int a3=3; int a4=6;
+		System.out.println("동일타입변수 = "+a0+","+a1+","+a2+","+a3+","+a4);
+		int [] a =new int [5];
+		a[0]=8; a[1]=5; a[2]=2; a[3]=3; a[4]=6;
+		System.out.println("동일탕비배열 = "+a0+","+a1+","+a2+","+a3+","+a4);
+	}
+}
