7c7ef99289024cb5180fb419eab4f17ecdb0b15d
Twodawn <48268804+Twodawn@users.noreply.github.com>
2019-10-17 15:28:27 +0900

    Create W7P6.java

---
+++ week7/W7P6.java
@@ -0,0 +1,15 @@
+package lecture7;
+
+public class Ex6_9 {
+
+	public static void main(String[] args) {
+		System.out.println("20161450 이새벽");
+		double x,y;
+		
+		x=10.0;
+		y=20.0;
+		double mult_value = x*y;
+		
+		System.out.println("곱하기 = "+mult_value);
+	}
+}
