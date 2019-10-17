f85596f5f07870def94600cc94a3600d03984d98
Twodawn <48268804+Twodawn@users.noreply.github.com>
2019-10-17 15:27:40 +0900

    Create W7P3.java

---
+++ week7/W7P3.java
@@ -0,0 +1,15 @@
+package lecture7;
+
+public class Ex6_7 {
+
+	public static void main(String[] args) {
+		System.out.println("20161450 이새벽");
+		double x,y;
+		
+		x=10.0;
+		y=20.0;
+		double sub_value = x-y;
+		
+		System.out.println("빼기 = "+sub_value);
+	}
+}
