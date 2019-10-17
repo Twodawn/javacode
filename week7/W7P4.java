0b40349aaf755c7ee58592e457c5f35920d7a182
Twodawn <48268804+Twodawn@users.noreply.github.com>
2019-10-17 15:27:55 +0900

    Create W7P4.java

---
+++ week7/W7P4.java
@@ -0,0 +1,22 @@
+package lecture7;
+
+public class Ex6_8 {
+
+	public static void main(String[] args) {
+		System.out.println("20161450 이새벽");
+		double x,y;
+		
+		x=10.0;
+		y=20.0;
+		double sub_value = fn_sub(x,y);
+		
+		System.out.println("빼기 = "+sub_value);
+	}
+	
+	public static double fn_sub(double x, double y)
+	{
+		double result;
+		result = x-y;
+		return result;
+	}
+}
